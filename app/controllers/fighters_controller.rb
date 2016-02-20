4dd737f498aed04b9855076240d240317f185c0f
kacper331 <denebv201@gmail.com>
2016-02-20 12:17:14 +0100

    Add create method to FightersController

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,4 +1,20 @@
 class FightersController < ApplicationController
   def new
+    @fighter = Fighter.new
+  end
+
+  def create
+    @fighter = Fighter.new(fighter_params)
+    if @fighter.save
+      redirect_to root_path, notice: "New fighter is ready to battle!"
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def fighter_params
+    params.require(:fighter).permit(:firstname, :lastname, :description)
   end
 end
