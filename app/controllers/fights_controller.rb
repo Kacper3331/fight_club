00a6efea501cb8c6ffc455e9e830d9465b2e7d88
kacper331 <denebv201@gmail.com>
2016-02-24 17:20:49 +0100

    Add method in Fight controller that adds data to result_id column

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -8,6 +8,8 @@ class FightsController < ApplicationController
     first_fighter_id = fight_params[:first_fighter_id].to_i
     second_fighter_id = fight_params[:second_fighter_id].to_i
     Result.create_result(first_fighter_id, second_fighter_id)
+    Fight.add_result_id(@fight, Result.last.id)
+
     if @fight.save
       redirect_to new_fights_path, notice: 'DUEL END!'
     else
