2c96c55333df3ac44e50657d0edcb69141f5e4ce
kacper331 <denebv201@gmail.com>
2016-02-24 17:03:31 +0100

    Add scope to Fight model to get info about the fighter

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -3,4 +3,5 @@ class Fight < ActiveRecord::Base
 
   scope :select_skill, -> (fighter_id) { Skill.where(fighter_id: fighter_id).sample }
 
+  scope :fighter_info, -> (fighter_id) { Fighter.where(id: fighter_id).first }
 end
