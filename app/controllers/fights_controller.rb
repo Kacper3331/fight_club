0386c60c11d4af41cc386fd7f1d3009b8b0e27b8
kacper331 <denebv201@gmail.com>
2016-02-24 16:58:23 +0100

    Add methods to create new fight in Fights controller

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,4 +1,19 @@
 class FightsController < ApplicationController
   def new
+    @fight = Fight.new
+  end
+
+  def create
+    @fight = Fight.new(fight_params)
+    if @fight.save
+      redirect_to new_fights_path, notice: 'DUEL END!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def fight_params
+    params.require(:fight).permit(:first_fighter_id, :second_fighter_id)
   end
 end
