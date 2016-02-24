15fd856736e7546087baea5dc12dd748e57d4f98
kacper331 <denebv201@gmail.com>
2016-02-24 17:05:50 +0100

    Add method to Result model which calculate's fighter attack

--- app/models/result.rb
+++ app/models/result.rb
@@ -7,4 +7,7 @@ class Result < ActiveRecord::Base
     Fight.fighter_info(fighter_id)
   end
 
+  def self.fighter_attack(skill_level, exp_points)
+    Fight.calculate_attack(skill_level, exp_points)
+  end
 end
