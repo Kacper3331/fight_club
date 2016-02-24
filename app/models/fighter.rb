8c071a05b03a15c1464beacc304a3468167c54d9
kacper331 <denebv201@gmail.com>
2016-02-24 08:02:39 +0100

    Add skills validation in fighter model

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -2,6 +2,8 @@ class Fighter < ActiveRecord::Base
   has_many :skills
   accepts_nested_attributes_for :skills, reject_if: :all_blank, allow_destroy: true
 
+  validates :skills, presence: { message: 'You need to add skills' }
+
   validates :firstname,
     presence: true
 
