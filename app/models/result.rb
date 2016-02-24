ba861602d98fdf9aa32d90f6688281e2dc3a7c28
kacper331 <denebv201@gmail.com>
2016-02-24 17:16:04 +0100

    Add method that create new result record and insert data to it

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,17 @@
 class Result < ActiveRecord::Base
+  def self.add_data(winner_id, loser_id, winner_skill, loser_skill, winner_attack, loser_attack)
+    result = Result.new
+    result.winner_id = winner_id
+    result.loser_id = loser_id
+    result.winner_skill_id = winner_skill.id
+    result.loser_skill_id = loser_skill.id
+    result.winner_attack = winner_attack
+    result.loser_attack = loser_attack
+    result.save
+    Fighter.add_exp_points(fighter_info(winner_id), 10)
+    Fighter.add_exp_points(fighter_info(loser_id), 5)
+  end
+
   def self.select_skill(fighter_id)
     Fight.select_skill(fighter_id)
   end
