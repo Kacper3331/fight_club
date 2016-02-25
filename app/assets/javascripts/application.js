d66d7e277dd31b670e2e88a1afed973d9a11ce2f
kacper331 <denebv201@gmail.com>
2016-02-25 11:50:29 +0100

    Add two tables to show fighters with highest w/l ratio and most powerful attack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,38 @@ $(document).ready(function() {
     } );
   } ).draw();
 } );
+
+
+$(document).ready(function() {
+  var table = $('#most_win_lose').DataTable({
+    "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+    "order": [[ 3, "desc" ]]
+  });
+
+  table.on( 'order.dt search.dt', function () {
+    table.column(0, {order:'applied'}).nodes().each( function (cell, i) {
+        cell.innerHTML = i+1;
+    } );
+  } ).draw();
+} );
+
+$(document).ready(function() {
+  var table = $('#most_pow_attack').DataTable({
+    "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+    "order": [[ 3, "desc" ]]
+  });
+
+  table.on( 'order.dt search.dt', function () {
+    table.column(0, {order:'applied'}).nodes().each( function (cell, i) {
+        cell.innerHTML = i+1;
+    } );
+  } ).draw();
+} );
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,14 +1,27 @@
 class StatisticsController < ApplicationController
-  helper_method :count_fights
+  helper_method [:count_fights, :averange_wins, :strongest_attack]
 
   def index
-    @fighters = Fighter.all
+    @fighters = Fighter.where('exp_points > ?', 100)
   end
 
   private
+
   def count_fights(fighter_id)
     count_wins = Result.where(winner_id: fighter_id).count
     count_loses = Result.where(loser_id: fighter_id).count
     @count_fights = Fighter.count_fights(count_wins, count_loses)
+  end
+
+  def strongest_attack(fighter_id)
+    count_wins = Result.where(winner_id: fighter_id).count
+    count_loses = Result.where(loser_id: fighter_id).count
+    @strongest_attack = Fighter.strongest_attack(fighter_id)
+  end
+
+  def averange_wins(fighter_id)
+    count_wins = Result.where(winner_id: fighter_id).count
+    count_loses = Result.where(loser_id: fighter_id).count
+    @averange_fights = count_wins > 0 && count_loses > 0 ? Fighter.averange_fights(count_wins, count_loses) : 0
   end
 end
