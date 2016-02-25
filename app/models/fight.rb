7cdd7964d67b046a277bc16b6caa62040b90eca0
kacper331 <denebv201@gmail.com>
2016-02-25 17:48:47 +0100

    Add fields validation to Fight model

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,6 +1,12 @@
 class Fight < ActiveRecord::Base
   has_many :fighters
 
+  validates :first_fighter_id,
+    presence: true
+
+  validates :second_fighter_id,
+    presence: true
+
   scope :select_skill, -> (fighter_id) { Skill.where(fighter_id: fighter_id).sample }
 
   scope :fighter_info, -> (fighter_id) { Fighter.where(id: fighter_id).first }
