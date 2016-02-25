21a4712bab65c98e6d0c7ecb64e8a429617ca432
kacper331 <denebv201@gmail.com>
2016-02-25 12:51:36 +0100

    Add avatar validation to fighter model

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -5,6 +5,10 @@ class Fighter < ActiveRecord::Base
 
   accepts_nested_attributes_for :skills, reject_if: :all_blank, allow_destroy: true
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_presence :avatar
+
   validates :skills, presence: { message: 'You need to add skills' }
 
   validates :firstname,
