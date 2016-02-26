ab406e8cf72ba2f036871ca848e006af38e342bf
kacper331 <denebv201@gmail.com>
2016-02-26 22:58:21 +0100

    Change email address where email will be send

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -5,6 +5,6 @@ class WelcomeMailer < ApplicationMailer
     @fighter = fighter
     @skills = Skill.where(fighter_id: @fighter.id)
     @job = Job.where(id: @fighter.job_id).first
-    mail(to: 'denebv201@gmail.com', subject: 'Welcome to our fighting World!')
+    mail(to: @fighter.email, subject: 'Welcome to our fighting World!')
   end
 end
